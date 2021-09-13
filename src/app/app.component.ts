814ebb49c89a739a01e7c2a04e78dfeae1864b76
abdulrahimkakkat <abdulrahim.elab@gmail.com>
2021-09-13 23:16:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent    {
   title = 'mytask';
+  firstName: string;
+lastName: string;
+siblings: Array<string>;
+  //constructor(private fb: FormBuilder) {}
+  ngOnInit(){
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.siblings = new Array<string>('Jane', 'Jack', 'Sophie');
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './app/child/child.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
